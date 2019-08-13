bdd5a0ff67b738b2d7ca802e69c41b2347057c6e
shrey <shreystechtips@gmail.com>
2019-08-12 19:49:31 -0700

    Initial Commit

---
+++ src/components/Header/HeaderLinks.jsx
@@ -0,0 +1,172 @@
+/*!
+
+=========================================================
+* Material Kit React - v1.7.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/material-kit-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+/*eslint-disable*/
+import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
+// react components for routing our app without refresh
+import { Link } from "react-router-dom";
+
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Tooltip from "@material-ui/core/Tooltip";
+
+// @material-ui/icons
+import { People, LibraryMusic, Event, Money, CalendarToday, ContactPhone } from "@material-ui/icons";
+
+// core components
+import CustomDropdown from "components/CustomDropdown/CustomDropdown.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+
+import headerLinksStyle from "assets/jss/material-kit-react/components/headerLinksStyle.jsx";
+import Header from "components/Header/Header.jsx";
+function HeaderFull() {
+  return (
+    <Header
+      color="transparent"
+      routes={dashboardRoutes}
+      brand="Supporters of NHS Orchestras"
+      rightLinks={<HeaderLinks />}
+      fixed
+      changeColorOnScroll={{
+        height: 400,
+        color: "white"
+      }}
+    // {...rest}
+    />
+  );
+}
+
+function HeaderLinks({ ...props }) {
+  const { classes } = props;
+  return (
+    <List className={classes.list}>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          title="Knights in Vienna"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            to="/"
+            href="/kiv"
+            tag={Link}
+            color="transparent"
+            className={classes.navLink}>
+            <Event className={classes.icons} /> KIV
+        </Button>
+        </Tooltip>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <CustomDropdown
+          noLiPadding
+          buttonText="About Us"
+          buttonProps={{
+            className: classes.navLink,
+            color: "transparent"
+          }}
+          buttonIcon={People}
+          dropdownList={[
+            <Link to="/" className={classes.dropdownLink}>
+              Officers
+            </Link>,
+            <Link to="/" className={classes.dropdownLink}>
+              Our Conductor
+          </Link>,
+            <Link to="/" className={classes.dropdownLink}>
+              Honors and Accomplishments
+          </Link>
+          ]}
+        />
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          title="See us in action!"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            to="/"
+            href="/performances"
+            tag={Link}
+            color="transparent"
+            className={classes.navLink}>
+            <LibraryMusic className={classes.icons} /> Performances
+        </Button>
+        </Tooltip>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Button
+          to="/"
+          href="/calendar"
+          tag={Link}
+          color="transparent"
+          className={classes.navLink}>
+          <Event className={classes.icons} /> Calendar
+        </Button>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Button
+          to="/"
+          href="/contact"
+          tag={Link}
+          color="transparent"
+          className={classes.navLink}>
+          <ContactPhone className={classes.icons} /> Contact
+        </Button>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="instagram-tooltip"
+          title="Follow us on instagram"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            color="transparent"
+            href="https://www.instagram.com/newport_orchestra"
+            target="_blank"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fab fa-instagram"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          title="We greatly appreciate your donations!"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            to="/"
+            href="/donate"
+            tag={Link}
+            color="transparent"
+            className={classes.navLink}>
+            <Money className={classes.icons} /> Donate
+        </Button>
+        </Tooltip>
+      </ListItem>
+    </List>
+  );
+}
+
+export default withStyles(headerLinksStyle)(HeaderLinks);
---
+++ src/views/LandingPage/LandingPage.jsx
@@ -0,0 +1,93 @@
+/*!
+
+=========================================================
+* Material Kit React - v1.7.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/material-kit-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+
+// @material-ui/icons
+
+// core components
+import Header from "components/Header/Header.jsx";
+import Footer from "components/Footer/Footer.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+import HeaderLinks from "components/Header/HeaderLinks.jsx";
+
+import Parallax from "components/Parallax/Parallax.jsx";
+
+import landingPageStyle from "assets/jss/material-kit-react/views/landingPage.jsx";
+
+// Sections for this page
+import ProductSection from "./Sections/ProductSection.jsx";
+import TeamSection from "./Sections/TeamSection.jsx";
+import WorkSection from "./Sections/WorkSection.jsx";
+
+const dashboardRoutes = [];
+
+class LandingPage extends React.Component {
+  render() {
+    const { classes, ...rest } = this.props;
+    return (
+      <div>
+        <Header
+          color="transparent"
+          routes={dashboardRoutes}
+          brand="Supporters of NHS Orchestras"
+          rightLinks={<HeaderLinks />}
+          fixed
+          changeColorOnScroll={{
+            height: 400,
+            color: "white"
+          }}
+          {...rest}
+        />
+        <Parallax filter image={require("assets/img/landing-bg.jpg")}>
+          <div className={classes.container}>
+            <GridContainer>
+              <GridItem>
+                <div className={classes.brand}>
+                  <h1 className={classes.title}>Newport High School Orchestra</h1>
+
+                </div>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </Parallax>
+        <div className={classNames(classes.main, classes.mainRaised)}>
+          <div className={classes.container}>
+            <ProductSection />
+            <TeamSection />
+            <WorkSection />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+LandingPage.propTypes = {
+  classes: PropTypes.object
+};
+
+export default withStyles(landingPageStyle)(LandingPage);
